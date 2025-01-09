1d76c318417c3d1dc07719e796964a301011bf90
qwe123456789123 <p1199019@naver.com>
2025-01-09 10:23:23 +0900

    포켓몬 클론코딩 프로젝트

--- src/main/java/org/koreait/produt/constants/DiscountType.java
+++
@@ -1,7 +0,0 @@
-package org.koreait.produt.constants;
-
-public enum DiscountType {
-    NONE, // 할인 없음
-    FIXED, // 정가 할인
-    RATE // 할인율
-}
--- src/main/java/org/koreait/produt/repositories/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package org.koreait.produt.repositories;
-
-import org.koreait.produt.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface ProductRepository extends JpaRepository<Product, Long>, QuerydslPredicateExecutor<Product> {
-}
